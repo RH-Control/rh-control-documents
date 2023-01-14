09999593ee87d19e31a06a069b6f1d13b26b7bed
Matheus Montenegro <matheusalbuquerquemontenegro@hotmail.com>
2023-01-14 14:37:23 -0300

    first commit

---
+++ src/main/java/br/com/ifpe/rhcontroldocuments/RhControlDocumentsApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.rhcontroldocuments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhControlDocumentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhControlDocumentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/rhcontroldocuments/RhControlDocumentsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.rhcontroldocuments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhControlDocumentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
